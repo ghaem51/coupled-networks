9cf26f9e63b8cfa17522f42d07270876f06977c7
ghaem <ghaem51@yahoo.com>
2022-02-01 02:56:59 +0330

    change directory

--- src/utilities/compare_networks.py
+++ src/utilities/compare_networks.py
@@ -1,7 +1,7 @@
 import networkx as nx
 
-network_a_location = "/Users/veneman/workspace/rise/coupled-networks/data/networks/generated/network_Polish_2383nodes_2.422deg_0.0rw_12345seed_mat.edgelist"
-network_b_location = "/Users/veneman/workspace/rise/coupled-networks/data/networks/generated/network_Polish_2383nodes_2.422deg_0.1rw_12345seed.edgelist"
+network_a_location = "/Users/ghaemarasteh/Desktop/uni/UniMaster/Term3/Seminar/project/coupled-networks/data/networks/generated/network_Polish_2383nodes_2.422deg_0.0rw_12345seed_mat.edgelist"
+network_b_location = "/Users/ghaemarasteh/Desktop/uni/UniMaster/Term3/Seminar/project/coupled-networks/data/networks/generated/network_Polish_2383nodes_2.422deg_0.1rw_12345seed.edgelist"
 
 a = nx.read_edgelist(network_a_location, nodetype=int)
 b = nx.read_edgelist(network_b_location, nodetype=int)
